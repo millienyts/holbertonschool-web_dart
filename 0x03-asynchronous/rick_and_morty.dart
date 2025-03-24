12930f7ce2ec98fdd71be544bb2d4b26834504b4
Millienyts <tmillieny@gmail.com>
2025-03-23 20:15:07 -0400

    asynchronous

--- 0x03-asynchronous/rick_and_morty.dart
+++ 0x03-asynchronous/rick_and_morty.dart
@@ -1,18 +1,19 @@
-import "package:http/http.dart" as http;
+import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 Future<void> printRmCharacters() async {
   try {
-    final res = await http.get(Uri.parse('https://rickandmortyapi.com/api/character'));
+    final response = await http.get(Uri.parse('https://rickandmortyapi.com/api/character'));
 
-    if (res.statusCode != 200) {
-      throw Exception("API error: ${res.statusCode}");
-    }
+    if (response.statusCode == 200) {
+      Map<String, dynamic> jsonData = jsonDecode(response.body);
+      List<dynamic> characters = jsonData['results'];
 
-    var jsonData = jsonDecode(res.body);
-
-    for (var character in jsonData['results']) {
-      print(character['name']);
+      for (var character in characters) {
+        print(character['name']);
+      }
+    } else {
+      throw Exception('Failed to load characters with status code: ${response.statusCode}');
     }
   } catch (err) {
     print('error caught: $err');
