b448614c18d4a1067d1ea80234d68a2dc6d7c323
Millienyts <tmillieny@gmail.com>
2025-03-23 20:13:41 -0400

    asynchronous

--- 0x03-asynchronous/rick_and_morty.dart
+++ 0x03-asynchronous/rick_and_morty.dart
@@ -1,14 +1,18 @@
 import "package:http/http.dart" as http;
 import 'dart:convert';
 
-printRmCharacters() async {
+Future<void> printRmCharacters() async {
   try {
-    final res = await http.get(
-      Uri.parse('https://rickandmortyapi.com/api/character'),
-    );
-    var json = jsonDecode(res.body);
-    for (var idx = 0; idx < json['results'].length; idx++) {
-      print("${json['results'][idx]['name']}");
+    final res = await http.get(Uri.parse('https://rickandmortyapi.com/api/character'));
+
+    if (res.statusCode != 200) {
+      throw Exception("API error: ${res.statusCode}");
+    }
+
+    var jsonData = jsonDecode(res.body);
+
+    for (var character in jsonData['results']) {
+      print(character['name']);
     }
   } catch (err) {
     print('error caught: $err');
